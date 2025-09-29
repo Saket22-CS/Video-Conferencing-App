098699186cb5fc0ea5a33e97be4953a976874650
Saket Chaudhary <saketrishu64821@gmail.com>
2025-09-29 13:28:52 +0530

    implement upcoming and previous meeting page

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import CallList from "@/components/CallList";
 
-const previous = () => {
+const PreviousPage = () => {
   return (
-    <section  className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold ">
-        Previous
-      </h1>
-    </section>
-  )
-}
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">Previous Calls</h1>
 
-export default previous
+      <CallList type="ended" />
+    </section>
+  );
+};
+
+export default PreviousPage;
